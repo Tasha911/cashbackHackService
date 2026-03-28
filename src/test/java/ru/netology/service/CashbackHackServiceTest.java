03743348c0663cad43d7b2025cecb31be8a1c5bb
“Natalia <fa.nat.ka911@gmail.com>
2026-03-29 00:00:32 +0700

    add Test

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -8,5 +8,12 @@ public class CashbackHackServiceTest {
 
     @Test
     public void remain() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 900;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 100;
+
+        assertEquals(expected, actual);
     }
 }
