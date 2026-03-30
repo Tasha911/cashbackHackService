b1e53b59f8c229f603b7ffbbd7f8727792b68079
“Natalia <fa.nat.ka911@gmail.com>
2026-03-30 22:13:27 +0700

    add noRemain test

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -14,4 +14,14 @@ public class CashbackHackServiceTest {
 
         assertEquals(actual, expected);
     }
+    @org.testng.annotations.Test
+    public void noRemain() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
 }
